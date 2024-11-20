ceabfa87a9bb61f2f5ea37a0c32ee5e42a764b7f
Thiago Janz <thiagojanz@hotmail.com>
2024-11-20 20:22:15 -0300

    Atualização App.js

--- src/App.js
+++ src/App.js
@@ -65,7 +65,7 @@ const App = () => {
           <Route path="/order-confirmation" element={<OrderConfirmation />} />
           <Route path="/confirmacao" element={<Confirmacao />} />
           <Route path="/produtos/marca/:brandId" element={<ProductsByBrand />} />
-          <Route path="*" element={<Navigate to="/" />} />
+          <Route path="%" element={<Navigate to="/" />} />
         </Routes>
       </div>
       <Footer />
---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ErrorPage() {
+  return (
+    <div>
+      <h1>Página não encontrada!</h1>
+      <p>A página que você está procurando não existe.</p>
+      <a href="/">Voltar para a home</a>
+    </div>
+  );
+}
+
+export default ErrorPage;
