b81ddd6273b50cbcbe8fffda9fc102e1af875244
Thiago Janz <thiagojanz@hotmail.com>
2025-01-14 19:36:26 -0300

    Atualização Globa e ForgotPassword

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -2,13 +2,11 @@ import React, { useState } from "react";
 import axios from "axios";
 import { Api_VariavelGlobal } from '../global';
 import { Form, Input, Button, Alert } from 'antd'; 
-import { useNavigate } from 'react-router-dom'; // Importando o hook useHistory para redirecionamento
 
 const ForgotPassword = ({ closeModal }) => {  // closeModal será a função que vai fechar o modal
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
-  const history = useNavigate(); // Hook para redirecionamento
-
+  
   // Função que será chamada ao enviar o formulário
   const handleSubmit = async (values) => {
     const { email } = values;
@@ -22,7 +20,6 @@ const ForgotPassword = ({ closeModal }) => {  // closeModal será a função que
       if (response.status === 200 || response.status === 201) {
         setMessage(response.data.message || "E-mail enviado com sucesso.");
         if (closeModal) closeModal();
-        history('/reset_senha'); // Redireciona para a página de redefinição de senha
       } else {
         throw new Error(response.data.message || "Erro desconhecido.");
       }
@@ -41,28 +38,34 @@ const ForgotPassword = ({ closeModal }) => {  // closeModal será a função que
     <div style={{ maxWidth: 400, margin: "0 auto", padding: "20px" }}>
       <h2 style={{ textAlign: "center" }}>Esqueci minha Senha</h2>
       <Form
-        name="forgot-password"
-        onFinish={handleSubmit} // Usando onFinish ao invés de onSubmit
-        layout="vertical"
-      >
-        <Form.Item
-          label="E-mail"
-          name="email"
-          rules={[{ required: true, message: "Por favor, insira seu e-mail!" }]}
-        >
-          <Input
-            type="email"
-            placeholder="Digite seu e-mail"
-            required
-          />
-        </Form.Item>
+  name="forgot-password"
+  onFinish={handleSubmit} // Usando onFinish ao invés de onSubmit
+  layout="vertical"
+>
+  <Form.Item
+    label="E-mail"
+    name="email"
+    rules={[{ required: true, message: "Por favor, insira seu e-mail!" }, { type: "email", message: "E-mail inválido!" }]}
+  >
+    <Input
+      type="email"
+      placeholder="Digite seu e-mail"
+    />
+  </Form.Item>
 
-        <Form.Item>
-          <Button className="buy-button-2" type="primary" htmlType="submit" block loading={loading} disabled={loading} onClick={handleSubmit}>
-            {loading ? "Enviando..." : "Enviar Link de Redefinição"}
-          </Button>
-        </Form.Item>
-      </Form>
+  <Form.Item>
+    <Button 
+      className="buy-button-2" 
+      type="primary" 
+      htmlType="submit" 
+      block 
+      loading={loading} 
+      disabled={loading}
+    >
+      {loading ? "Enviando..." : "Enviar Link de Redefinição"}
+    </Button>
+  </Form.Item>
+</Form>
 
       {message && (
         <Alert
