11aab40ee97292bd00133e63630711f65efbeec9
Thiago Janz <thiagojanz@hotmail.com>
2025-01-14 17:58:41 -0300

    Atualização global

--- src/global.js
+++ src/global.js
@@ -1,3 +1,3 @@
 // globals.js
-export const Api_VariavelGlobal = 'https://api-bebilogo-krwt.onrender.com';
+export const Api_VariavelGlobal = "https://api-bebilogo-krwt.onrender.com";
 //export const Api_VariavelGlobal = 'http://localhost:4000';
